e96ab26f3f0a80bc0b67b70e2abcc32a58087055
Jose Pardo <josmar.pm19@gmail.com>
2022-04-10 22:56:20 -0500

    add floating action  buttons

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -11,10 +11,19 @@ export const ContadorScreen = () => {
         Contador: { contador }
       </Text>
       <TouchableOpacity
+        style={styles.fabLocationBR}
         onPress={() => setContador(contador + 1)}
       >
-        <View style={styles.buttonIncrease}>
-          <Text>+1</Text>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.fabLocationBL}
+        onPress={() => setContador(contador - 1)}
+      >
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -31,8 +40,27 @@ const styles = StyleSheet.create({
     fontSize: 40,
     top: -15
   },
-  buttonIncrease:{
-    backgroundColor: 'red',
-    borderRadius: 100
+  fabLocationBR:{
+    position: 'absolute',
+    bottom: 25,
+    right: 25
+  },
+  fabLocationBL:{
+    position: 'absolute',
+    bottom: 25,
+    left: 25
+  },
+  fab:{
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center'
+  },
+  fabText:{
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center'
   }
 })
