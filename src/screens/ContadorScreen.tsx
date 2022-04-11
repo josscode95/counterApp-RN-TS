aca3b6d4b38339fb6ad77b9aa330cc1ddd733211
Jose Pardo <josmar.pm19@gmail.com>
2022-04-10 22:45:58 -0500

    add styleSheet

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -1,33 +1,38 @@
 import React, { useState } from 'react'
-import { Text, View, TouchableOpacity } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
 
 export const ContadorScreen = () => {
 
   const [contador, setContador] = useState(0)
 
   return (
-    <View 
-      style={{
-        flex: 1,
-        justifyContent: 'center'
-      }}
-    >
-      <Text 
-        style={{
-          textAlign: 'center',
-          fontSize: 40,
-          top: -15
-        }}
-      >
+    <View style={styles.container}>
+      <Text style={styles.title}>
         Contador: { contador }
       </Text>
       <TouchableOpacity
         onPress={() => setContador(contador + 1)}
       >
-        <View>
+        <View style={styles.buttonIncrease}>
           <Text>+1</Text>
         </View>
       </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  title:{
+    textAlign: 'center',
+    fontSize: 40,
+    top: -15
+  },
+  buttonIncrease:{
+    backgroundColor: 'red',
+    borderRadius: 100
+  }
+})
